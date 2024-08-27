180523b2806c462ee39fcf619bf2703902e9d7a6
Jake B <jacobboxerman@gmail.com>
2024-08-27 15:42:37 -0500

    Cleanup testimonials, trying out ref

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,22 @@ import SpeechDescriptions from "./components/SpeechDescriptions";
 import Testimonials from "./components/Testimonials";
 import FeaturedMedia from "./components/FeaturedMedia";
 
+import { useRef } from "react";
+
 export default function App() {
+  const testimonialsRef = useRef(null);
+
   return (
     <div className="bg-slate-50">
       <Hero />
       <CompaniesScroller />
       <Quote />
-      <Navbar />
+      <Navbar testRef={testimonialsRef} />
       <Summary />
       <Videos />
       <SpeechDescriptions />
       <FeaturedMedia />
-      <Testimonials />
+      <Testimonials ref={testimonialsRef} />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,90 @@
+// import React from "react";
+
+// const sections = [
+//   {
+//     name: "Summary",
+//     id: "summary",
+//   },
+//   {
+//     name: "Speaking Videos",
+//     id: "videos",
+//   },
+//   {
+//     name: "Speech Descriptions",
+//     id: "speeches",
+//   },
+//   {
+//     name: "Testimonials",
+//     id: "testimonials",
+//   },
+//   {
+//     name: "Featured Media",
+//     id: "media",
+//   },
+// ];
+
+// const handleScroll = (ref) => {
+//   window.scrollTo({
+//     top: ref.offsetTop,
+//     left: 0,
+//     behavior: "smooth",
+//   });
+// };
+
+// function Navbar({ testRef }) {
+//   return (
+//     <div className="bg-slate-900 text-white sticky top-[-0.5px]">
+//       <ul className="flex gap-3 md:gap-6 justify-center items-center text-center h-14 text-sm md:text-xl 2xl:text-2xl">
+//         {sections.map((s, i) => {
+//           return (
+//             <li key={i}>
+//               <a href={s.id} onClick={() => handleScroll(testRef.current)}>{s.name}</a>
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default Navbar;
+
 import React from "react";
 
 const sections = [
-  {
-    name: "Summary",
-    id: "summary",
-  },
-  {
-    name: "Speaking Videos",
-    id: "videos",
-  },
-  {
-    name: "Speech Descriptions",
-    id: "speeches",
-  },
-  {
-    name: "Testimonials",
-    id: "testimonials",
-  },
-  {
-    name: "Featured Media",
-    id: "media",
-  },
+  { name: "Summary", id: "summary" },
+  { name: "Speaking Videos", id: "videos" },
+  { name: "Speech Descriptions", id: "speeches" },
+  { name: "Testimonials", id: "testimonials" },
+  { name: "Featured Media", id: "media" },
 ];
 
-function Navbar() {
+const handleScroll = (ref) => {
+  if (ref && ref.current) {
+    window.scrollTo({
+      top: ref.current.offsetTop - 10,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
+};
+
+function Navbar({ testRef }) {
   return (
     <div className="bg-slate-900 text-white sticky top-[-0.5px]">
       <ul className="flex gap-3 md:gap-6 justify-center items-center text-center h-14 text-sm md:text-xl 2xl:text-2xl">
-        {sections.map((s, i) => {
-          return (
-            <li key={i}>
-              <a href={`#${s.id}`}>{s.name}</a>
-            </li>
-          );
-        })}
+        {sections.map((s, i) => (
+          <li key={i}>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                handleScroll(testRef);
+              }}
+            >
+              {s.name}
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
