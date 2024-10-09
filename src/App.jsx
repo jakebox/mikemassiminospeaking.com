c2636acea485b15c7664cb3ec5ca60fa40386237
Jake B <jacobboxerman@gmail.com>
2024-10-09 19:23:44 -0400

    Fix Back to Top button

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,9 @@ export default function App() {
         <SpeechDescriptions ref={speechDescriptionsRef} />
         <FeaturedMedia ref={featuredMediaRef} />
         <Testimonials ref={testimonialsRef} />
-        <p className="text-black text-center pb-5"><a href='/#'>Back to Top</a></p>
+        <p className="text-black text-center pb-5">
+          <button onClick={() => topRef.current.scrollIntoView()}>Back to Top</button>
+        </p>
       </div>
       <Footer />
     </>
